b10a117e382c659be80b9381bb05d18781bf2f8f
zt_diaoxr <diaoxinrui@qq.com>
2019-06-12 11:36:44 +0800

    1

---
+++ src/ajax/axiosConfig.js
@@ -0,0 +1,44 @@
+module.exports = {
+  url: "http://acaas-gateway.standard/abside/abs-te-ib-ownservices/getRuleList",
+  params: {
+    "SYS_HEAD": {
+      "ConsumerId": "066",
+      "ServiceUrl": "10.16.2.124",
+      "RequestDate": "20190528",
+      "RequestTime": "104323",
+      "ConsumerSeqNo": "06600000000000000000000000000000",
+      "ServiceNo": null,
+      "BagSeqNo": "",
+      "ConsumerIP": "20.3.7.122",
+      "AppNum": "TE",
+      "ClientType": "110064",
+      "Reserve": ""
+    },
+    "APP_HEAD": {
+      "UserNum": "203917",
+      "UserName": "老周",
+      "BranchNum": "2039",
+      "BranchName": "四川农信测试机构76",
+      "DvcNum": "8C-EC-4B-B7-10-91",
+      "MacValue": "",
+      "DeviceNum": "",
+      "DeviceModel": "",
+      "Tlr": "",
+      "FictTellerNum": "",
+      "LegalPersonNum": "001"
+    },
+    "Body": {
+      "RuleLocate": "1",
+      "TranCode": "00301012",
+      "TranNm": "子账户销户",
+      "LegalPersonNum": "001",
+      "BranchLevel": "3",
+      "BranchAttr": "2",
+      "TlrCstNum": null,
+      "BranchNum": "2039",
+      "UserNum": "203917",
+      "AppNum": "TE",
+      "Tp": "[\"NI\",\"CV\",\"CI\",\"BN\",\"FR\",\"AU\",\"DS\"]"
+    }
+  }
+}
