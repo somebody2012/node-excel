e6a96e88bfcf0d5727bf31366d46da8d37db96e4
zt_diaoxr <diaoxinrui@qq.com>
2019-09-23 13:55:23 +0800

    1

--- src/genCommSql/index.js
+++ src/genCommSql/index.js
@@ -1,6 +1,6 @@
 
-var filename = "./菜单.xlsx";
-var sheetName = "Sheet1";
+var filename = "./1326修改个人客户信息_in.xlsx";
+var sheetName = "Sheet2";
 
 var xlsx = require('node-xlsx');
 var fs = require("fs");
@@ -10,26 +10,13 @@ var utils = require("../utils/index");
 var excelPath = path.resolve(__dirname,filename);
 var workSheets = xlsx.parse(fs.readFileSync(excelPath));
 var curWorkSheet = workSheets.find(v => v.name.includes(sheetName)).data;
+utils.transformEmpty(curWorkSheet);
 
-
-for(var i=0;i<curWorkSheet.length;i++){
-  curWorkSheet[i][31] || (curWorkSheet[i][31] = ''); 
-  curWorkSheet[i][32] || (curWorkSheet[i][32] = ''); 
-  curWorkSheet[i][33] || (curWorkSheet[i][33] = ''); 
-  for(var j=0;j<curWorkSheet[i].length;j++){
-    var item = curWorkSheet[i][j];
-    if(item === 0){
-      curWorkSheet[i][j] = '0';
-    }else if(!item){
-      curWorkSheet[i][j] = '';
-    }
-  }
-}
 
 
 
 var oringinData = [
-  {tableName:"IB_UPM_MENU_INFO",data:curWorkSheet.slice(1)}
+  {tableName:"TP_CIP_INTERFACECOLMAP",data:curWorkSheet}
 ];
 
 var insertSql = utils.genInsertSql(oringinData)
---
+++ src/server/dist/static/js/158.js
@@ -0,0 +1,1 @@
+webpackJsonp([158],{"56C0":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("transition",{attrs:{name:"fade-transform",mode:"out-in"}},[t("router-view")],1)},staticRenderFns:[]},a=n("/Xao")({computed:{}},r,!1,null,null,null);t.default=a.exports}});
