3540d6e820057fd731c4070fca59cc98f249a543
zt_diaoxr <diaoxinrui@qq.com>
2019-10-21 19:31:32 +0800

    4

--- src/auth/index_rewite2.js
+++ src/auth/index_rewite2.js
@@ -630,7 +630,8 @@ class Auth {
     var col19 = ""; // 通过人脸识别时授权级别 
     var col20 = curSheetRows[0][20]; // 规则确认人 
     var row = [col0,col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15,col16,col17,col18,col19,col20];
-    if(col2 == '09908001') return;
+    var notNeedPubCond = ['09908001','09908002','09908003','09908004','09908005','09908006','09908007'];
+    if(notNeedPubCond.includes(col2)) return;
     this.dynamicWorkSheet.push(row);
   }
   // 改为非强制条件
